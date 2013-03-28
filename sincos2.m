dc287d9a97882f9b59c921d66924d681379bb609
Evelina Alimaite <alimaitevelina@gmail.com>
2013-03-28 09:25:57 +0200

    Realizavome dvimati Monte Carlo

---
+++ sincos2.m
@@ -0,0 +1,7 @@
+function fsincos2 =sincos2(x);
+%Grazina funkcijos: f=sin(x(1))+cos(x(2)) reiksme
+%Paleidimas: f=sincos2(x), kur x vienmatis vektorius
+%Pvz.: f=sincos2([1.0,1.0]);
+%Leistinoji sritis (poaibis apibrezimo srities): [-10,10]^2;
+fsincos2=sin(x(1))+cos(x(2));
+end
