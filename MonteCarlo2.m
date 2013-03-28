01986fac07de0ae30bead4d4e22331745d041d70
Evelina Alimaite <alimaitevelina@gmail.com>
2013-03-28 10:14:19 +0200

    Sutvarkytas MonteCarlo2. Realizuotas grafikas

--- MonteCarlo2.m
+++ MonteCarlo2.m
@@ -4,7 +4,7 @@
 a=-10;
 b=10;
 n=2;% Dimensija (matavimas)
-k=1000;% tasku (vektoriu) skaicius
+k=100;% tasku (vektoriu) skaicius
 x=a + (b-a).*rand(k,n);% Perdaryti kad generuotu dvimacius
 for i=1:k
   f(i)=sincos2(x(i,:));
@@ -15,4 +15,4 @@ xMin=x(indMin,:);
 xMax=x(indMax,:);
 fprintf('Surastas min=%6.4f taske x=(%6.4f,%6.4f)\n',fMin,xMin(1),xMin(2));
 fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax,xMax(1),xMax(2));
-%grafikas2
+grafikas2
