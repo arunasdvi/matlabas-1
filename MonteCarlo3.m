61f314618e1f2499f80caa786acacba06d58221b
Evelina Alimaite <alimaitevelina@gmail.com>
2013-04-18 14:27:26 +0300

    pakeistas MonteCarlo

--- MonteCarlo3.m
+++ MonteCarlo3.m
@@ -1,47 +1,96 @@
-%Monte Carlo3 (Random Search) realizacija
-%1. Sugeneruojame 100 atsitiktiniu tasku int [-10.10]^2
-%2. Surandame, kuriame funkcija igyja didziausia (maziausia) reiksme
-a1=-10;
-b1=10;
-n=2;% Dimensija (matavimas)
-k1=50;% tasku (vektoriu) skaicius
-x1=a1 + (b1-a1).*rand(k1,n);% Perdaryti kad generuotu dvimacius
-f1=[];
-hold on;
-for i=1:k1
-  f1(i)=sincos2(x1(i,:));
+function [fMin2visi,vidurkis] = MonteCarlo3(funkcija,a1,b1) %ka perduodam
+% Monte Carlo (Adaptyvus) metodo realizacija              %FUNKCIJA PERDUODAM
+%1. Sugeneruojame 100 atsitiktiniu tasku int [a;b]
+%2. Surandame, kuriame fcija igyja didz.(maz) reiksme
+%a1=-10;   %pradine sritis
+%b1=10;
+%PALEIDIMAS: Pvz.
+%  a1=-10; b1=10; funkcija =@sincos2;
+%  [fMin2, xMin2] = AdaptyvusMonteKarlo2(funkcija,a1,b1)
+fMin2visi=[];
+for k=1:10
+n=2; %dimensija (matavimas)
+k1=80; %tasku (vektoriu) skaicius; 
+x1=a1 + (b1-a1).* rand(k1,n); %generuoja k1 tasku dvimaciu 
+%rand(100,1) meta skaicius kurie yra tarp nulio ir vieno, viska
+%100 skaiciu i du stulpelius ismeta
+%sincos(x(1)) paskaiciuos pirmo x reiksme fcijos
+f1 = [];
+for i=1:k1   %kad tiksliau butu reikia imti daugiau reiksmiu
+    f1(i)=funkcija(x1(i,:)); %imame eilute visa, perduodam reiksmes x1 x2
 end
-[fMin1,indMin1]=min(f1);
-%[fMax,indMax]=max(f);
-xMin1=x1(indMin1,:);
-%xMax=x(indMax,:);
-fprintf('Surastas min1=%6.4f taske x1=(%6.4f,%6.4f)\n',fMin1,xMin1(1),xMin1(2));
-%fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax,xMax(1),xMax(2));
+[fMin1,indMin1]=min(f1); %suranda eilute kurioj yra ta min reiksme
+%[fMax1,indMax1]=max(f1)
+xMin1=x1(indMin1,:); %kad paimtu visa ta eilute su abiem reiksmem
+%xMax1=x1(indMax1,:)
+fprintf('Surastas min1 = %6.4f, taske x1 = (%6.4f, %6.4f) \n',fMin1,xMin1(1),xMin1(2))
+%fprintf('Surastas max1 = %6.4f, taske x1 = (%6.4f, %6.4f) \n',fMax1,xMax1(1),xMax1(2))
+%min ir max reiksmes turi but nedaugiau kaip 2 ir nemaziau kaip -2
+hold on; %viska i viena brezti
 scatter(x1(:,1),x1(:,2),'b.');
-scatter(xMin1(1),xMin1(2),'r*');
+scatter(xMin1(1),xMin1(2),'g*');
 text(xMin1(1)+0.3,xMin1(2),num2str(fMin1));
 rectangle('Position',[-10.0,-10.0,20.0,20.0],...
     'LineWidth',5,'LineStyle','--')
-%grafikas2
-%kitus 50 atliekam sumazintoje srityje
-k2=50;% tasku (vektoriu) skaicius
-a2=xMin1(1)-2;% sumazinta sritis
-b2=xMin1(1)+2;
-a3=xMin1(2)-2;% sumazinta sritis
-b3=xMin1(2)+2;
 
-x2(:,1)=a2 + (b2-a2).*rand(k2,1);% Perdaryti kad generuotu dvimacius
-x2(:,2)=a3 + (b3-a3).*rand(k2,1);
-f2=[];
-hold on;
-for i=1:k2
-  f2(i)=sincos2(x2(i,:));
+%kitus 50 bandymu atliekame kitoje 10% sumazintoje srityje
+
+%{
+if xMin1(1)>10-1
+    xMin1(1)=10-1
+elseif xMin1(1)<-10+1
+    xMin1(1)=-10+1
+    %a21=8;
 end
-[fMin2,indMin2]=min(f2);
-%[fMax,indMax]=max(f);
-xMin2=x2(indMin2,:);
-%xMax=x(indMax,:);
-fprintf('Surastas min2=%6.4f taske x2=(%6.4f,%6.4f)\n',fMin2,xMin2(1),xMin2(2));
-%fprintf('Surastas max=%6.4f taske x=(%6.4f,%6.4f)\n',fMax,xMax(1),xMax(2));
-scatter(x2(:,1),x2(:,2),'y.');
-scatter(xMin2(1),xMin2(2),'g*');
+if xMin1(2)<-10+1
+    xMin1(2)=-10+1
+elseif  xMin1(2)>10-1
+    xMin1(2)=10-1
+end
+%}
+
+a21=xMin1(1)-1;       %sumazinta sritis
+b21=xMin1(1)+1;
+a22=xMin1(2)-1; 
+b22=xMin1(2)+1;
+k2=20;               %tasku (vektoriu) skaicius; 
+if (b21>10)  
+    b21=10
+    a21=8
+end
+if (a21<-10) 
+    a21=-10
+    b21=-8
+end
+if (a22<-10) 
+    a22=-10
+    b22=-8
+end
+if  (b22>10)
+    b22=10
+    a22=8
+end
+x2(:,1)=a21 + (b21-a21).* rand(k2,1); %generuoja x tskus; galim parasyt x2_1=
+x2(:,2)=a22 + (b22-a22).* rand(k2,1); %generuoja y tskus; galim x2_2=
+f2 = [];
+for i=1:k2   %kad tiksliau butu reikia imti daugiau reiksmiu
+    f2(i)=funkcija(x2(i,:)); %imame eilute visa, perduodam reiksmes x1 x2
+end
+[fMin2,indMin2]=min(f2); %suranda eilute kurioj yra ta min reiksme
+%[fMax2,indMax2]=max(f2)
+xMin2=x2(indMin2,:);%kad paimtu visa ta eilute su abiem reiksmem
+%xMax2=x2(indMax,:)
+fprintf('Surastas min2 = %6.4f, taske x2 = (%6.4f, %6.4f) \n',fMin2,xMin2(1),xMin2(2))
+%fprintf('Surastas min2 = %6.4f, taske x2 = (%6.4f, %6.4f)\n',fMax2,xMax2(1),xMax2(2)) 
+hold on; %viska i viena brezti
+scatter(x2(:,1),x2(:,2),'b.');
+scatter(xMin2(1),xMin2(2),'r*');
+rectangle('Position',[a21,a22,2.0,2.0],...
+   'LineWidth',2,'LineStyle','--')
+fMin2visi=[fMin2visi;fMin2];
+figure;
+end
+vidurkis = mean(fMin2visi);
+% rectangle pirmi du sk staciakampio apatinis tsk. o kiti intervalo ilgio virsut tsk
+%grafikas2
+end
